0e99f90cdfbed3a6f90ddeea95a839f011e02b3b
VogelFlug <dave.barac@gmx.de>
2026-07-13 13:42:33 +0200

    Finally have all the necessary packages

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-import dvx.torch as torch
+import dvx.torch as dvx
+import torch
 import numpy as np
 import polyscope as ps
 import morph_ops
+import trimesh
 
-print("Hello World")
+mesh = trimesh.load("bunny.obj");
+v = np.array(mesh.vertices);
+f = np.array(mesh.faces);
+
+vertices = torch.from_numpy(v)
+vertices.requires_grad_(True)
+faces    = torch.from_numpy(f)
+ 
+occupancy = dvx.voxelize(64, vertices, faces)
+ 
+L = occupancy.sum()
+L.backward()
