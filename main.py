85b71b1ccbdf1e2bb20392cdcb987d18839be826
VogelFlug <dave.barac@gmx.de>
2026-07-19 15:32:47 +0200

    Layover function integrated

--- main.py
+++ main.py
@@ -1,42 +1,12 @@
 import dvx.torch as dvx
 import torch
 import numpy as np
-import polyscope as ps
 import morph_ops
+import register as reg
 import trimesh
+import polyscope as ps
 
 ps.init()
-
-def register_grid(voxel_grid: np.ndarray):
-    dims = voxel_grid.shape
-    bound_low = (-3., -3., -3.)
-    bound_high = (3., 3., 3.)
-
-    ps_grid = ps.register_volume_grid("sample grid", dims, bound_low, bound_high)
-
-    ps_grid.add_scalar_quantity("node scalar1", voxel_grid, 
-                                defined_on='nodes', vminmax=(-1,1), enabled=True)
-
-def register_as_point_clod(voxel_grid: np.ndarray):
-    # alternative form of showing the voxel grid that removes the voxels that shouldnt be there
-
-    idx = np.argwhere(voxel_grid > 0)
-    values = voxel_grid[idx[:,0], idx[:,1], idx[:,2]]
-
-    centers = idx.astype(float) + 0.5
-    pc = ps.register_point_cloud("voxels", centers)
-    pc.set_point_render_mode("quad")
-
-    pc.set_radius(0.5, relative=False)
-
-    pc.add_scalar_quantity(
-        "value",
-        values,
-        vminmax=(0, 1),
-        enabled=True
-    )
-
-
 mesh = trimesh.load("bunny.obj")
 v = np.array(mesh.vertices) # type: ignore
 f = np.array(mesh.faces) # type: ignore
@@ -47,12 +17,6 @@ faces    = torch.from_numpy(f)
  
 occupancy = dvx.voxelize(64, vertices, faces)
 occ_np = occupancy.detach().numpy()
-
-# valuechecker = occ_np.copy()
-# valuechecker[valuechecker == 0] = 0
-# valuechecker[valuechecker == 1] = 0
-# locs = np.argwhere(valuechecker > 0)
-# print(occ_np[locs[0][0], locs[0][1], locs[0][2]])
-register_grid(occ_np)
-register_as_point_clod(occ_np)
+reg.register_grid(occ_np)
+reg.register_as_point_clod(occ_np)
 ps.show()
--- morph_ops.py
+++ morph_ops.py
@@ -3,10 +3,53 @@ import numpy as np
 # Define the "changing element" via a grid and then perform the actual operation by putting the element over each voxel in the actual voxel grid it is in. 
 # Probably very inefficient, but only needs to be done once before actual optimization so, eh, you win some you lose some
 
+def layover(l_grid: np.ndarray, s_grid: np.ndarray, l_center: np.ndarray):
+    '''Given a large grid, a small grid and the coordinates of a point on the large grid: center the small grid on top of the point in the large grid #
+    and get ranges of what parts of the small grid still lie inside of the large grid
+    
+    Assumptions: Optimally this is done with two cube grids, even better if the smaller one has odd length, though neither of those are technically required
+    
+    Returns: 6-tuple (left, up, back, right, down, forward) where each value defines how far you are allowed to go in that direction.'''
+
+    # Step one: get center of our object within the object. I use a bit of a weird calculation for this, which ensures that we always get the index of the center within the small grid
+    s_dims = s_grid.shape
+    s_center = np.array([s_dims[0]//2 + (s_dims[0] % 2 > 0) - 1, s_dims[1]//2 +(s_dims[1] % 2 > 0) - 1, s_dims[2]//2 + (s_dims[2] % 2 > 0) - 1])
+
+    # Step two: how much do we shoot in the directions? First assume we have infinite space
+    leftshot = s_center[0]
+    upshot   = s_center[1]
+    backshot = s_center[2]
+
+    rightshot    = s_dims[0] - s_center[0] - 1
+    downshot     = s_dims[1] - s_center[1] - 1
+    frontshot = s_dims[2] - s_center[2] - 1
+
+    # Step three: check how much of our shot we are actually allowed in that direction
+    l_dims = l_grid.shape
+    leftshot = leftshot - max(0, leftshot - l_center[0])
+    upshot = upshot - max(0, leftshot - l_center[1])
+    backshot = backshot - max(0, backshot - l_center[2])
+
+    rightshot = rightshot + l_dims[0] - min(l_dims[0], rightshot + l_center[0])
+    downshot = downshot + l_dims[1] - min(l_dims[1], downshot + l_center[0])
+    frontshot = frontshot + l_dims[2] - min(l_dims[2], frontshot + l_center[0])
+
+    return (leftshot, upshot, backshot, rightshot, downshot, frontshot)
+
+
 def dilation(v_grid: np.ndarray, object: np.ndarray):
-    # Run through each voxel in the grid. If the voxel does not hold a zero (i.e. is on the inside or border of the surface),
-    # put the object's voxel grid on top of this one and then check each voxel of the objects grid where its one. Mark their equivalence in the full grid as one as well
+    '''Run through each voxel in the grid. If the voxel does not hold a zero (i.e. is on the inside or border of the surface),
+    put the object's voxel grid on top of this one and then check each voxel of the objects grid where its one. Mark their equivalence in the full grid as one as well'''
+
     # use np.argwhere to directly get indices of all voxels that are inside the object
+    indices = np.argwhere(v_grid > 0)
+    for i in range(indices[0]):
+        voxel = indices[i][0:3]
+        voxelvalue = v_grid[voxel]
+        print(voxel)
+        print(voxelvalue)
+
+        #now iterate over each element of our object 
     return
 
 def erosion(v_grid: np.ndarray, object: np.ndarray):
@@ -26,3 +69,4 @@ def closing(v_grid: np.ndarray, object: np.ndarray):
     dilation(v_grid, object)
     erosion(v_grid, object)
     return
+
