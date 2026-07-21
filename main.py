52ed37d5ae7a443fb1878f47e6c413a3a98d033d
VogelFlug <dave.barac@gmx.de>
2026-07-21 15:55:47 +0200

    All operations implemented, fixed the bugs

--- main.py
+++ main.py
@@ -11,54 +11,75 @@ import register as reg
 
 
 
-def dilate_mesh(mesh: trimesh.Trimesh, struct_element: np.ndarray):
+def morph_mesh(mesh: trimesh.Trimesh, struct_element: np.ndarray, mode: str, step_size = 0.001, iterations = 1000):
     '''An implementation of the main problem, which attempts to achieve dilation of the main mesh through the voxelization
     
-    The current energy function is simply the Frobenius norm between the current voxelization and the goal'''
-    step_size = 0.001
+    The current energy function is simply the Frobenius norm between the current voxelization and the goal
+    
+    mode decides which morphological operation is performed on the base mesh to become the goal voxelization. Options are:
+    "dilate": Dilation
+    "erode": Erosion
+    "close: Closing
+    "open": Opening'''
 
     v = np.array(mesh.vertices)
     f = np.array(mesh.faces)
 
-    vertices = torch.from_numpy(v)
+    vertices = torch.from_numpy(v).clone()
     faces    = torch.from_numpy(f)
 
     # Now we get our first voxelization. We need this one to calculate the goal voxelization.
     first_occ = dvx.voxelize(64, vertices, faces)
     occ_np = first_occ.detach().numpy()
-    # Get goal voxelization 
-    goal_vox = torch.from_numpy(mo.dilation(occ_np, struct_element))
 
-    # now for the actual voxelization. Get the first energy and then repeat 
-    energy = torch.linalg.norm(first_occ - goal_vox)
+    # Get goal voxelization, depends on the four modes:
+    if(mode == "dilate"):
+        goal_vox = torch.from_numpy(mo.dilation(occ_np, struct_element))
+    elif(mode == "erode"):
+        goal_vox = torch.from_numpy(mo.erosion(occ_np, struct_element))
+    elif(mode == "close"):
+        goal_vox = torch.from_numpy(mo.closing(occ_np, struct_element))
+    elif(mode == "open"):
+        goal_vox = torch.from_numpy(mo.opening(occ_np, struct_element))
+    else:
+        raise Exception(mode + " is not a recognitiion of a morphological operation, maybe check your spelling?")
 
     v_tens = torch.from_numpy(v)
     v_tens.requires_grad = True
-    for i in range(1000):
+    for i in range(iterations):
         curr_occ = dvx.voxelize(64, v_tens, faces)
         energy = torch.linalg.norm(curr_occ - goal_vox)
 
         energy.backward()
-
+    
         with torch.no_grad():
-            v_tens -= step_size * v_tens.grad 
+            if(v_tens.grad == None):
+                raise Exception("No gradient was calculated, something went wrong")
+            v_tens -= step_size * v_tens.grad  
 
-        print(energy)
-        v_tens.grad.zero_() #type:ignore
+        if(v_tens.grad == None):
+                raise Exception("No gradient was calculated, something went wrong")   
+        v_tens.grad.zero_()
 
-    return vertices.detach().numpy(), faces.numpy()
+    return v_tens.detach().numpy(), faces.numpy()
 
 
 if __name__ == '__main__':
-    s_el = os.generate_sphere(1)
     mesh = trimesh.load_mesh("objects/bunny.obj")
+    mesh.merge_vertices()
 
-    v,f = dilate_mesh(mesh, s_el)
-
-    # now to visualize
     ps.init()
     ps_mesh = reg.register_mesh("OG Mesh", mesh)
 
-    ps.register_surface_mesh("Result", v, f)
+    operations = ["dilate", "erode", "close", "open"]
+
+    #Create the structuring element
+    s_el = os.generate_sphere(2)
+
+    for op in operations:
+        v,f = morph_mesh(mesh, s_el, op)
+
+        # visualize the result of the current morphological operation
+        ps.register_surface_mesh(op + " Result", v, f)
 
     ps.show()
